77794ec25a50431567546107c79ffd1dbea6fc7e
Jochem Klaver <global-php@7ochem.nl>
2018-04-30 13:31:51 +0200

    Wrote class body for EmailAddress and Url ValueObjects

--- src/ValueObject/EmailAddress.php
+++ src/ValueObject/EmailAddress.php
@@ -2,12 +2,176 @@
 
 namespace GlobalPhp\Entities\ValueObject;
 
+/**
+ * Email Address Value Object
+ *
+ * @author Jochem Klaver <global-php@7ochem.nl>
+ */
 class EmailAddress implements ValueObjectInterface
 {
     use ValueObjectTrait;
 
     /**
-     * @todo Use parse_url('mailto://'.$value) and implement getters and setters (with...) for all RELEVANT parts
+     * @var string
      */
+    private $name = '';
+
+    /**
+     * @var string
+     */
+    private $localPart = '';
+
+    /**
+     * @var string
+     */
+    private $domain = '';
+
+    /**
+     * Email Address Value Object constructor
+     *
+     * @param mixed $value
+     */
+    public function __construct($value)
+    {
+        $this->extractParts($value);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->combineParts();
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return static
+     */
+    public function withName($name)
+    {
+        $newEmailAddress = clone $this;
+        $newEmailAddress->name = $name;
+        return $newEmailAddress;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocalPart()
+    {
+        return $this->localPart;
+    }
+
+    /**
+     * @param string $localPart
+     * @return static
+     */
+    public function withLocalPart($localPart)
+    {
+        $newEmailAddress = clone $this;
+        $newEmailAddress->localPart = $localPart;
+        return $newEmailAddress;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+
+    /**
+     * @param string $domain
+     * @return static
+     */
+    public function withDomain($domain)
+    {
+        $newEmailAddress = clone $this;
+        $newEmailAddress->domain = $domain;
+        return $newEmailAddress;
+    }
+
+    /**
+     * @param string|string[] $value
+     */
+    private function extractParts($value)
+    {
+        // $value is an array of string parts
+        if (is_array($value)) {
+            if (!array_key_exists('name', $value) && !array_key_exists('local_part', $value)
+                && !array_key_exists('domain', $value)) {
+                throw new \InvalidArgumentException('Passed email address value array does not contain any valid parts');
+            }
+            if (array_key_exists('name', $value)) {
+                $this->name = $value['name'];
+            }
+            if (array_key_exists('local_part', $value)) {
+                $this->localPart = $value['local_part'];
+            }
+            if (array_key_exists('domain', $value)) {
+                $this->domain = $value['domain'];
+            }
+            return;
+        }
+
+        // $value must be a string
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException('Passed email address value is not a string or array of parts');
+        }
+        $value = trim($value);
+        if ('' === $value) {
+            throw new \InvalidArgumentException('Passed email address value is empty');
+        }
+
+        // Handle '"Display Name" <local-part@domain>' format
+        if ('>' === substr($value, -1)) {
+            if (false === strpos($value, '<')) {
+                throw new \InvalidArgumentException('Invalid email address value of format \'"Display Name" <local-part@domain>\'');
+            }
+            if ('"' === substr($value, 0, 1)) {
+                if (false === strpos($value, '"', 1)) {
+                    throw new \InvalidArgumentException('Invalid display name in email address value of format \'"Display Name" <local-part@domain>\'');
+                }
+                $this->name = substr($value, 1, strpos($value, '"', 1) - 1);
+                $value = substr(ltrim(substr($value, strpos($value, '"', 1) + 1)), 1, -1);
+            } else {
+                $this->name = rtrim(substr($value, 0, strpos($value, '<')));
+                $value = substr($value, strpos($value, '<') + 1, -1);
+            }
+        }
+
+        // Extract local part and domain ('local-part@domain')
+        if (false === strrpos($value, '@')) {
+            throw new \InvalidArgumentException('Invalid email address value: value does not contain @-symbol');
+        }
+
+        $this->domain = substr($value, strrpos($value, '@') + 1);
+        $this->localPart = substr($value, 0, strrpos($value, '@'));
+
+        return;
+    }
+
+    /**
+     * @return string
+     */
+    private function combineParts()
+    {
+        return sprintf(
+            $this->name !== '' ? '"%s" <%s@%s>' : '%s%s@%s',
+            $this->name,
+            $this->localPart,
+            $this->domain
+        );
+    }
 
 }
--- src/ValueObject/Url.php
+++ src/ValueObject/Url.php
@@ -2,12 +2,167 @@
 
 namespace GlobalPhp\Entities\ValueObject;
 
+/**
+ * URL Value Object
+ *
+ * @author Jochem Klaver <global-php@7ochem.nl>
+ * @see parse_url()
+ * @see http://php.net/parse-url
+ * @see https://en.wikipedia.org/wiki/Uniform_Resource_Identifier
+ */
 class Url implements ValueObjectInterface
 {
     use ValueObjectTrait;
 
     /**
-     * @todo Use parse_url() and implement getters and setters (with...) for all parts
+     * @var string|null
      */
+    private $scheme;
+
+    /**
+     * @var bool
+     */
+    private $isSchemeSlashed = false;
+
+    /**
+     * @var string
+     */
+    private $host = '';
+
+    /**
+     * @var int|null
+     */
+    private $port;
+
+    /**
+     * @var string|null
+     */
+    private $user;
+
+    /**
+     * @var string|null
+     */
+    private $pass;
+
+    /**
+     * @var string|null
+     */
+    private $path;
+
+    /**
+     * @var string|null
+     */
+    private $query;
+
+    /**
+     * @var string|null
+     */
+    private $fragment;
+
+    /**
+     * URL Value Object constructor
+     *
+     * @param mixed $value
+     */
+    public function __construct($value)
+    {
+        $this->extractParts($value);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->combineParts();
+    }
+
+    /**
+     * @todo `getX` and `withX` for all parts
+     * @todo `getQueryParts()` using {@see parse_str()}
+     */
+
+    /**
+     * @param string|string[] $value
+     */
+    private function extractParts($value)
+    {
+        // $value is an array of string parts
+        if (is_array($value)) {
+            $partNames = ['scheme', 'host', 'port', 'user', 'pass', 'path', 'query', 'fragment'];
+            $validParts = array_intersect_key($value, array_fill_keys($partNames, ''));
+            if (0 === count($validParts)) {
+                throw new \InvalidArgumentException('Passed URL value array does not contain any valid parts');
+            }
+            foreach ($partNames as $partName) {
+                if (array_key_exists($partName, $value)) {
+                    $this->$partName = $value[$partName];
+                }
+            }
+            return;
+        }
+
+        // $value must be a string
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException('Passed URL value is not a string or array of parts');
+        }
+        $value = ltrim($value);
+        if ('' === $value) {
+            throw new \InvalidArgumentException('Passed URL value is empty');
+        }
+
+        // Extract parts
+        $parts = parse_url($value);
+        if (false === $parts) {
+            throw new \InvalidArgumentException('Passed URL value is invalid');
+        }
+        foreach ($parts as $partName => $partValue) {
+            $this->$partName = $partValue;
+        }
+        $this->isSchemeSlashed = (strpos($value, '//') === 0
+            || $this->scheme && strpos($value, $this->scheme . '://') === 0);
+    }
+
+    /**
+     * @param null|string $component
+     * @return string
+     */
+    private function combineParts($component = null)
+    {
+        switch ($component) {
+            case 'scheme':
+                return ($this->scheme ? $this->scheme . ':' : '')
+                    . ($this->isSchemeSlashed ? '//' : '');
+
+            case 'authority':
+                if (!$this->host) {
+                    return '';
+                }
+                return ($this->user ?? '')
+                    . ($this->pass ? ':' . $this->pass : '')
+                    . ($this->user || $this->pass ? '@' : '')
+                    . $this->host
+                    . (is_int($this->port) ? ':' . $this->port : '');
+
+            case 'path':
+                return $this->path ?? '';
+
+            case 'query':
+                return $this->query ? '?' . $this->query : '';
+
+            case 'fragment':
+                return $this->fragment ? '#' . $this->fragment : '';
+
+            case null:
+                return $this->combineParts('scheme')
+                    . $this->combineParts('authority')
+                    . $this->combineParts('path')
+                    . $this->combineParts('query')
+                    . $this->combineParts('fragment');
+
+            default:
+                throw new \InvalidArgumentException('Unknown component argument');
+        }
+    }
 
 }
